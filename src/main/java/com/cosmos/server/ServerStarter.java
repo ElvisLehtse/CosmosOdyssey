fefdd658669e69156b49980e2b10587ad3d53476
ElvisLehtse <elehtse@gmail.com>
2024-06-28 11:35:20 +0300

    Finalized

--- src/main/java/com/cosmos/server/ServerStarter.java
+++ src/main/java/com/cosmos/server/ServerStarter.java
@@ -2,8 +2,11 @@ package com.cosmos.server;
 
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Scanner;
 
 /**
  * This class starts the localhost server.
@@ -11,14 +14,16 @@ import java.net.InetSocketAddress;
 public class ServerStarter {
 
     public void startServer() {
-        final int port = 8080;
+        File file = new File("Localhost port.txt");
         try {
+            Scanner scanner = new Scanner(file);
+            int port = Integer.parseInt(scanner.nextLine().replace("port=", ""));
             HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
             RequestHandler requestHandler = new RequestHandler();
             requestHandler.requestGetAndPost(server, "/");
             server.start();
         } catch (IOException e) {
-            System.out.println(STR."\{e.getMessage()} Could not create the server");
+            System.out.println(e.getMessage());
         }
     }
 }
